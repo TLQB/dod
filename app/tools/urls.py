bfd6ff698efa7e5cbaf042412ccea0fe712d00d9
Tran Le Quy Bao <baotlq@vacsvnhcm.local>
2024-10-02 15:34:16 +0700

    add tool update discount rate

--- app/tools/urls.py
+++ app/tools/urls.py
@@ -4,6 +4,9 @@ from . import views
 
 
 urlpatterns = [
-    path('get-file-data/', views.get_file_data, name='get_file_data'),
-    path("export_data_excel/", views.process_excel_data, name='export_data_excel'),
+    path("get-file-data/", views.get_file_data, name="get_file_data"),
+    path("export_data_excel/", views.process_excel_data, name="export_data_excel"),
+    path(
+        "update_discount_rate/", views.update_discount_rate, name="update_discount_rate"
+    ),
 ]
